3368c85be0b87ada5d8af8c2857eb545f572defc
Lara Gabriela <lara_g_lopes@hotmail.com>
2024-01-24 12:51:41 -0300

    menu concluído

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     $("#menuIcon").click(function() {
         $(".bar:nth-child(1)").toggleClass("rotate-down");
         $(".bar:nth-child(2)").toggleClass("fade-out");
-        $(".bar:nth-child(3)").toggleClass("rotate-up");
+        $(".bar:nth-child(3)").toggleClass("rotate-down");
         $("#menuContent").slideToggle();
     });
 });
 
-function menuFn(x) {
+function clickMenu(x) {
     x.classList.toggle("change")
 };
 
