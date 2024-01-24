5f75a0d9227ee473389be06681730327fd4b2b8d
Lara Gabriela <lara_g_lopes@hotmail.com>
2024-01-24 12:08:11 -0300

    interação do menu

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#menuIcon").click(function() {
+        $(".bar:nth-child(1)").toggleClass("rotate-down");
+        $(".bar:nth-child(2)").toggleClass("fade-out");
+        $(".bar:nth-child(3)").toggleClass("rotate-up");
+        $("#menuContent").slideToggle();
+    });
+});
+
+function clickMenu(x) {
+    x.classList.toggle("change")
+};
+
+
+
